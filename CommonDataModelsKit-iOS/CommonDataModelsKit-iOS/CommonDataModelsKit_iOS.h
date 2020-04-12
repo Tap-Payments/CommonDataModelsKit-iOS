15343386f6600451316c7e35f5324947d4e3bc59
Tap Payments <o.rabie@tap.company>
2020-04-12 07:38:08 +0200

    1- Create podspec file

---
+++ CommonDataModelsKit-iOS/CommonDataModelsKit-iOS/CommonDataModelsKit_iOS.h
@@ -0,0 +1,19 @@
+//
+//  CommonDataModelsKit_iOS.h
+//  CommonDataModelsKit-iOS
+//
+//  Created by Osama Rabie on 12/04/2020.
+//  Copyright © 2020 Tap Payments. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CommonDataModelsKit_iOS.
+FOUNDATION_EXPORT double CommonDataModelsKit_iOSVersionNumber;
+
+//! Project version string for CommonDataModelsKit_iOS.
+FOUNDATION_EXPORT const unsigned char CommonDataModelsKit_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommonDataModelsKit_iOS/PublicHeader.h>
+
+
