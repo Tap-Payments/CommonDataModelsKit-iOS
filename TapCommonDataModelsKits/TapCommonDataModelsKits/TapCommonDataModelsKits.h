a00be643091b97a4ea36055a7e7291374ea9cb15
Tap Payments <o.rabie@tap.company>
2020-04-12 07:33:38 +0200

    1- Refactor up Card model, Country Enum and Currency enum

---
+++ TapCommonDataModelsKits/TapCommonDataModelsKits/TapCommonDataModelsKits.h
@@ -0,0 +1,19 @@
+//
+//  TapCommonDataModelsKits.h
+//  TapCommonDataModelsKits
+//
+//  Created by Osama Rabie on 12/04/2020.
+//  Copyright © 2020 Tap Payments. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TapCommonDataModelsKits.
+FOUNDATION_EXPORT double TapCommonDataModelsKitsVersionNumber;
+
+//! Project version string for TapCommonDataModelsKits.
+FOUNDATION_EXPORT const unsigned char TapCommonDataModelsKitsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TapCommonDataModelsKits/PublicHeader.h>
+
+
