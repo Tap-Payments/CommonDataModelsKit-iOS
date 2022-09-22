bb9374a95e75039d5b9dcd47081682d8f764953b
O Rabie <o.rabie@tap.company>
2022-09-22 08:55:19 +0200

    Fixed a bug in payment options response model

--- CommonDataModelsKit-iOS/CommonDataModelsKit-iOS/Core/models/APiModels/TapPaymentOptionsRequest/TapPaymentOptionsReponseModel.swift
+++ CommonDataModelsKit-iOS/CommonDataModelsKit-iOS/Core/models/APiModels/TapPaymentOptionsRequest/TapPaymentOptionsReponseModel.swift
@@ -1,176 +1,153 @@
 //
-//  TapLoyaltyModel.swift
-//  CommonDataModelsKit-iOS
+//  TapPaymentOptionsReponseModel.swift
+//  CheckoutSDK-iOS
 //
-//  Created by Osama Rabie on 13/09/2022.
+//  Created by Osama Rabie on 6/15/21.
+//  Copyright © 2021 Tap Payments. All rights reserved.
 //
 
 import Foundation
+import TapCardVlidatorKit_iOS
 
-/// Tap loyalty model.
-public struct TapLoyaltyModel : Codable {
+/// Payment Options Response model.
+public struct TapPaymentOptionsReponseModel: IdentifiableWithString {
     
-    /**
-     Tap loyalty model.
-     - Parameter id: The id of teh loyalty programe
-     - Parameter bankName: The bank identifier to match it with the bin response
-     - Parameter bankLogo: The icon of the loyalty program owner
-     - Parameter loyaltyProgramName: The name of the loyalty programe to be used in the header title
-     - Parameter loyaltyPointsName: The name of the used shceme (ADCB Points, Fawry Values, etc.)
-     - Parameter termsConditionsLink: A link to open the T&C of the loyalty scheme
-     - Parameter supportedCurrencies: The list of supported currencies each with the conversion rate
-     - Parameter transactionsCount: The total number of availle points
-     */
+    // MARK: - Internal -
+    // MARK: Properties
     
-    public init(id: String?, bankName: String? = nil , bankLogo: String? = nil , loyaltyProgramName: String? = nil , loyaltyPointsName: String? = nil , termsConditionsLink: String? = nil, supportedCurrencies: [LoyaltySupportedCurrency]? = nil, transactionsCount: String? = nil) {
-        self.id = id
-        self.bankName = bankName
-        self.bankLogo = bankLogo
-        self.loyaltyProgramName = loyaltyProgramName
-        self.loyaltyPointsName = loyaltyPointsName
-        self.termsConditionsLink = termsConditionsLink
-        self.supportedCurrencies = supportedCurrencies
-        self.transactionsCount = transactionsCount
+    /// Object identifier.
+    public let identifier: String
+    
+    /// Object type.
+    internal let object: String
+    
+    /// List of available payment options.
+    public var paymentOptions: [PaymentOption]
+    
+    /// Transaction currency.
+    public let currency: TapCurrencyCode
+    
+    /// Merchant iso country code.
+    internal let merchantCountryCode: String?
+    
+    /// Amount for different currencies.
+    public let supportedCurrenciesAmounts: [AmountedCurrency]
+    
+    /// Saved cards.
+    public var savedCards: [SavedCard]?
+    
+    /// Order data
+    public var order: Order?
+    
+    /// The dummy loyalty view model
+    public var loyaltyModel: TapLoyaltyModel?
+    
+    // MARK: - Private -
+    
+    private enum CodingKeys: String, CodingKey {
+        
+        case currency                   = "currency"
+        case identifier                 = "id"
+        case object                     = "object"
+        case paymentOptions             = "payment_methods"
+        case supportedCurrenciesAmounts = "supported_currencies"
+        
+        
+        case savedCards                 = "cards"
+        
+        case merchantCountryCode        = "country"
+        
+        case order                      = "order"
     }
     
-    /// The id of teh loyalty programe
-    public let id :String?
-    /// The bank identifier to match it with the bin response
-    public let bankName: String?
-    /// The icon of the loyalty program owner
-    public let bankLogo: String?
-    /// The name of the loyalty programe to be used in the header title
-    public let loyaltyProgramName: String?
-    /// The name of the used shceme (ADCB Points, Fawry Values, etc.)
-    public let loyaltyPointsName: String?
-    /// A link to open the T&C of the loyalty scheme
-    public let termsConditionsLink: String?
-    /// The list of supported currencies each with the conversion rate
-    public let supportedCurrencies: [LoyaltySupportedCurrency]?
-    /// The total number of availle points as a string formatted as it comes from backend
-    public let transactionsCount: String?
-    /// The total number of availle points in a numeric format for numbers processing
-    public var numericTransactionCount: Int {
-        return Int(transactionsCount?.filter("0123456789".contains) ?? "0") ?? 0
+    // MARK: Methods
+    
+    public init(identifier:                        String,
+                object:                            String,
+                paymentOptions:                    [PaymentOption],
+                currency:                          TapCurrencyCode,
+                supportedCurrenciesAmounts:        [AmountedCurrency],
+                savedCards:                        [SavedCard]?,
+                merchantCountryCode:               String?,
+                order:                             Order?,
+                loyalty:                           TapLoyaltyModel?) {
+        
+        self.identifier                     = identifier
+        self.object                         = object
+        self.paymentOptions                 = paymentOptions
+        self.currency                       = currency
+        self.supportedCurrenciesAmounts     = supportedCurrenciesAmounts
+        self.savedCards                     = savedCards
+        self.merchantCountryCode            = merchantCountryCode
+        self.order                          = order
+        self.loyaltyModel                   = loyalty
     }
 }
 
-// MARK: Welcome convenience initializers and mutators
-
-public extension TapLoyaltyModel {
-    ///Tap loyalty model. from given Data
-    /// - Parameter data: The data to decode it into the model
-    init(data: Data) throws {
-        self = try newJSONDecoder().decode(TapLoyaltyModel.self, from: data)
-    }
+// MARK: - Decodable
+extension TapPaymentOptionsReponseModel: Decodable {
     
-    ///Tap loyalty model. from given json string
-    /// - Parameter string: The string  json to decode it into the modelParameter data: The data to decode it into the model
-    /// - Parameter encoding: The encoding method. Default is UTF8
-    init(_ json: String, using encoding: String.Encoding = .utf8) throws {
-        guard let data = json.data(using: encoding) else {
-            throw NSError(domain: "JSONDecoding", code: 0, userInfo: nil)
+    public init(from decoder: Decoder) throws {
+        
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        let identifier                      = try container.decode(String.self, forKey: .identifier)
+        let object                          = try container.decode(String.self, forKey: .object)
+        var paymentOptions                  = try container.decode([PaymentOption].self, forKey: .paymentOptions)
+        let currency                        = try container.decode(TapCurrencyCode.self, forKey: .currency)
+        let supportedCurrenciesAmounts      = try container.decode([AmountedCurrency].self, forKey: .supportedCurrenciesAmounts)
+        var savedCards                      = try container.decodeIfPresent([SavedCard].self, forKey: .savedCards)
+        let merchantCountryCode             = try container.decodeIfPresent(String.self, forKey: .merchantCountryCode)
+        let order                           = try container.decodeIfPresent(Order.self, forKey: .order)
+        
+        
+        paymentOptions = paymentOptions.sorted(by: { $0.orderBy < $1.orderBy })
+        
+        for i in 0...paymentOptions.count-1 {
+            if paymentOptions[i].brand == .unknown {
+                if paymentOptions[i].paymentType == .Web {
+                    paymentOptions[i].brand = CardBrand.aiywaLoyalty
+                }
+            }
         }
-        try self.init(data: data)
-    }
-    
-    ///Tap loyalty model. from data/json in a given URL
-    init(fromURL url: URL) throws {
-        try self.init(data: try Data(contentsOf: url))
-    }
-    
-    /// Encodes the model into Data
-    func jsonData() throws -> Data {
-        return try newJSONEncoder().encode(self)
-    }
-    
-    
-    /// Encodes the model into pretty formatted JSON string
-    func jsonString(encoding: String.Encoding = .utf8) throws -> String? {
-        return String(data: try self.jsonData(), encoding: encoding)
-    }
-}
-
-// MARK: - SupportedCurrency
-/// The model of the loyalty supported currencies. Each one will have the the rating + the currency itself
-public struct LoyaltySupportedCurrency: Codable {
-    
-    /**
-     The model of the loyalty supported currencies. Each one will have the the rating + the currency itself
-     - Parameter currency: The currency itself
-     - Parameter balance: The balane in this currency
-     - Parameter minimumAmount: Minimum redemption value in this currency
-     */
-    
-    public init(currency: AmountedCurrency?, balanceAmount: Double?, minimumAmount: Double?) {
-        self.currency = currency
-        self.balanceAmount = balanceAmount
-        self.minimumAmount = minimumAmount
-    }
-    
-    /// The currency itself
-    public let currency: AmountedCurrency?
-    /// Balance in this currency
-    public let balanceAmount:Double?
-    /// minimum redemption value in this currency
-    public let minimumAmount:Double?
-}
-
-// MARK: SupportedCurrency convenience initializers and mutators
-
-public extension LoyaltySupportedCurrency {
-    /**
-     The model of the loyalty supported currencies. Each one will have the the rating + the currency itself
-     - Parameter data: The data to decode it into the model
-     */
-    init(data: Data) throws {
-        self = try newJSONDecoder().decode(LoyaltySupportedCurrency.self, from: data)
-    }
-    
-    /**
-     The model of the loyalty supported currencies. Each one will have the the rating + the currency itself
-     - Parameter string: The string  json to decode it into the modelParameter data: The data to decode it into the model
-     - Parameter encoding: The encoding method. Default is UTF8
-     */
-    init(_ json: String, using encoding: String.Encoding = .utf8) throws {
-        guard let data = json.data(using: encoding) else {
-            throw NSError(domain: "JSONDecoding", code: 0, userInfo: nil)
-        }
-        try self.init(data: data)
-    }
-    
-    /**
-     The model of the loyalty supported currencies. Each one will have the the rating + the currency itself
-     */
-    init(fromURL url: URL) throws {
-        try self.init(data: try Data(contentsOf: url))
-    }
-    
-    /// Encodes the model into Data
-    func jsonData() throws -> Data {
-        return try newJSONEncoder().encode(self)
-    }
-    
-    /// Encodes the model into pretty formatted JSON string
-    func jsonString(encoding: String.Encoding = .utf8) throws -> String? {
-        return String(data: try self.jsonData(), encoding: encoding)
+        
+        paymentOptions = paymentOptions.filter { ($0.brand != .unknown || $0.paymentType == .ApplePay) }
+        paymentOptions = paymentOptions.sorted(by: { $0.orderBy < $1.orderBy })
+        
+        // Filter saved cards based on allowed card types passed by the user when loading the SDK session
+        let merchnantAllowedCards = SharedCommongDataModels.sharedCommongDataModels.allowedCardTypes
+        savedCards = savedCards?.filter { (merchnantAllowedCards.contains($0.cardType ?? CardType(cardType: .All))) }
+        
+        self.init(identifier:                   identifier,
+                  object:                       object,
+                  paymentOptions:               paymentOptions,
+                  currency:                     currency,
+                  supportedCurrenciesAmounts:   supportedCurrenciesAmounts,
+                  savedCards:                   savedCards,
+                  merchantCountryCode:          merchantCountryCode,
+                  order:                        order,
+                  loyalty:                      .init(id: "", bankName: "ADCB", bankLogo: "https://is1-ssl.mzstatic.com/image/thumb/Purple126/v4/78/00/ed/7800edd0-5854-b6ce-458f-dfcf75caa495/AppIcon-0-0-1x_U007emarketing-0-0-0-5-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/1024x1024.jpg", loyaltyProgramName: "ADCB TouchPoints", loyaltyPointsName: "TouchPoints", termsConditionsLink: "https://www.adcb.com/en/personal/adcb-for-you/touchpoints/touchpoints-rewards.aspx", supportedCurrencies: [.init(currency: AmountedCurrency.init(.AED, 1000, "", 2, 50), balanceAmount: 1000, minimumAmount: 100),.init(currency: AmountedCurrency.init(.EGP, 5000, "", 2, 10), balanceAmount: 5000, minimumAmount: 500)], transactionsCount: "50.000"))
     }
 }
 
-// MARK: - Helper functions for creating encoders and decoders
-
-func newJSONDecoder() -> JSONDecoder {
-    let decoder = JSONDecoder()
-    if #available(iOS 10.0, OSX 10.12, tvOS 10.0, watchOS 3.0, *) {
-        decoder.dateDecodingStrategy = .iso8601
+/// Responsible for helpers method to access data inside the model
+extension TapPaymentOptionsReponseModel {
+    
+    /**
+     Gets a specific payment option by id
+     - Parameter with: The id of the needed payment option
+     - Returns: Payment option if found with the specified id, else nil
+     */
+    public func fetchPaymentOption(with id:String) -> PaymentOption? {
+        var requiredPaymentOption:PaymentOption?
+        
+        // Let us get the needed payment option if any
+        let filteredPaymentOptions:[PaymentOption] = paymentOptions.filter{ $0.identifier == id }
+        // Make sure at least one option met the requirement
+        guard !filteredPaymentOptions.isEmpty else { return requiredPaymentOption }
+        // That is it..
+        requiredPaymentOption = filteredPaymentOptions.first
+        
+        return requiredPaymentOption
     }
-    return decoder
-}
-
-func newJSONEncoder() -> JSONEncoder {
-    let encoder = JSONEncoder()
-    if #available(iOS 10.0, OSX 10.12, tvOS 10.0, watchOS 3.0, *) {
-        encoder.dateEncodingStrategy = .iso8601
-    }
-    return encoder
 }
