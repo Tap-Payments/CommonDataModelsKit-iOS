466f8ce6d0322e0f911586591b1c6371c8d362a3
MahmoudAllam <m.allam@tap.company>
2026-04-30 07:26:11 +0300

    Added SPM Support

---
+++ Package.swift
@@ -0,0 +1,38 @@
+// swift-tools-version: 5.8
+import PackageDescription
+
+let package = Package(
+    name: "CommonDataModelsKit-iOS",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "CommonDataModelsKit_iOS",
+            targets: ["CommonDataModelsKit_iOS"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Tap-Payments/TapAdditionsKitV2.git", from: "0.0.19"),
+        .package(url: "https://github.com/Tap-Payments/TapKeychainV2-iOS.git", from: "1.0.3"),
+        .package(url: "https://github.com/Tap-Payments/TapCardVlidatorKit-iOS.git", from: "1.0.24"),
+        .package(url: "https://github.com/TakeScoop/SwiftyRSA.git", from: "1.7.0")
+    ],
+    targets: [
+        .target(
+            name: "CommonDataModelsKit_iOS",
+            dependencies: [
+                .product(name: "TapAdditionsKitV2", package: "TapAdditionsKitV2"),
+                .product(name: "TapKeychainV2", package: "TapKeychainV2-iOS"),
+                .product(name: "TapCardVlidatorKit-iOS", package: "TapCardVlidatorKit-iOS"),
+                "SwiftyRSA"
+            ],
+            path: "CommonDataModelsKit-iOS/CommonDataModelsKit-iOS/Core",
+            resources: [
+                .process("assets")
+            ]
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
+
